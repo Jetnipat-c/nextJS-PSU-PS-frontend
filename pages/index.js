64942cfac301dd72e15beb7c85043def5e23ba7d
alonecheer <alonecheer@hotmail.com>
2020-03-29 16:36:01 +0700

    npm axios and set user-pass

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
+import { useState } from "react";
+import axios from "axios";
 function HomePage() {
+  const [username,setUsername] = useState('');
+  const [password,setPassword] = useState('');
   
+  const login = async () => {
+    
+  }
   return (
   <div>
     <h1>Login PSU!</h1>
     <form>
-      <h3>username : <input type="text" onChange=""></input> </h3>
-      <h3>password : <input type="text" onChange=""></input> </h3>
+      <h3>username : <input type="text" onChange={(e)=>setUsername(e.target.value)}></input> </h3>
+      <h3>password : <input type="text" onChange={(e)=>setPassword(e.target.value)}></input> </h3>
       <button onClick="">Login</button>
     </form>
   </div>
