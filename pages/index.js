7d179f7f0af4060f393601147a2cee601049858a
alonecheer <alonecheer@hotmail.com>
2020-03-30 11:13:12 +0700

    consol.log(profile)

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 function HomePage() {
   const [user,setUser] = useState('');
   const [pass,setPass] = useState('');
-  
+  const [profile, setProfile] = useState([]);
   const login = async () => {
     const result = await axios.post('http://localhost:3001/userloginpsu/login',
     { username: user, password: pass },)
@@ -11,12 +11,22 @@ function HomePage() {
     console.log(result.data)
     sessionStorage.setItem('login',JSON.stringify({user: result.data}))
   }
+
+  const getprofile = async (e) => {
+    const profile = sessionStorage.getItem('login',JSON.stringify({user}))
+    console.log('profile',profile)
+    //setProfile = profile
+    e.preventDefault()
+    //console.log(sessionStorage.getItem('login',JSON.stringify({user})))
+  }
   return (
   <div>
     <h1>Login PSU!</h1>
       <h3>username : <input type="text" onChange={(e)=>setUser(e.target.value)}></input> </h3>
       <h3>password : <input type="text" onChange={(e)=>setPass(e.target.value)}></input> </h3>
       <button type="submit" onClick={login}>Login</button>
+      <button type="submit" onClick={getprofile}>Get profile</button>
+      {profile}
   </div>
   )
 }
