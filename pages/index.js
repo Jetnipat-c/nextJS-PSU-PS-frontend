14444999984a3a0563a8a78a0519db38d21871cd
alonecheer <alonecheer@hotmail.com>
2020-03-30 10:53:12 +0700

    check error

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,22 @@
 import { useState } from "react";
 import axios from "axios";
 function HomePage() {
-  const [username,setUsername] = useState('');
-  const [password,setPassword] = useState('');
+  const [user,setUser] = useState('');
+  const [pass,setPass] = useState('');
   
   const login = async () => {
     const result = await axios.post('http://localhost:3001/userloginpsu/login',
-    { username: username, password: password },)
+    { username: user, password: pass },)
+    console.log("xxxxx")
     console.log(result.data)
     sessionStorage.setItem('login',JSON.stringify({user: result.data}))
   }
   return (
   <div>
     <h1>Login PSU!</h1>
-    <form>
-      <h3>username : <input type="text" onChange={(e)=>setUsername(e.target.value)}></input> </h3>
-      <h3>password : <input type="text" onChange={(e)=>setPassword(e.target.value)}></input> </h3>
+      <h3>username : <input type="text" onChange={(e)=>setUser(e.target.value)}></input> </h3>
+      <h3>password : <input type="text" onChange={(e)=>setPass(e.target.value)}></input> </h3>
       <button type="submit" onClick={login}>Login</button>
-    </form>
   </div>
   )
 }
