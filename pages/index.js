fce414099c65fff7b026f0f678d0880cb1551e85
alonecheer <alonecheer@hotmail.com>
2020-03-31 21:41:29 +0700

    clear

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,42 @@
 import LoginPage from '../src/components/Login'
-import { Button } from 'antd';
-import styled from 'styled-components';
-const StyledButton = styled(Button)`
-     color: red;
+import styled from 'styled-components'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+const StyledWrapper = styled.div`
+
 `
 function HomePage() {
+  const [user,setUser] = useState('');
+  const [pass,setPass] = useState('');
+  const [profile, setProfile] = useState();
 
+  useEffect(()=>{
+    getprofile();
+  },[])
+
+  const login = async () => {
+    const result = await axios.post('http://localhost:3001/userloginpsu/login',
+    { username: user, password: pass },)
+    console.log(result.data)
+    sessionStorage.setItem('login',JSON.stringify({user: result.data}))
+    
+  }
+
+  const getprofile = async () => {
+    const profiles = sessionStorage.getItem('login',JSON.stringify(user))
+    console.log('profiles',profiles)
+    setProfile(profiles)
+    //console.log('profile',profile)
+    //console.log(sessionStorage.getItem('login',JSON.stringify({user})))
+  }
   return (
   <div>
-    <h3>Test Ant design</h3>
-     <StyledButton>Press Me</StyledButton>
-    <LoginPage></LoginPage>
+    <h1>Login PSU!</h1>
+      <h3>username : <input type="text" onChange={(e)=>setUser(e.target.value)}></input> </h3>
+      <h3>password : <input type="text" onChange={(e)=>setPass(e.target.value)}></input> </h3>
+      <button type="submit" onClick={login}>Login</button>
+      <button type="submit" onClick={getprofile}>Get profile</button>
+      {profile}
   </div>
   )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,54 +1,31 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import styled from 'styled-components'
-
-const StyledWrapper = styled.div``
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+const StyledWrapper = styled.div`
+.bg-image {
+  /* The image used */
+  background-image: url("../images/bg-blur.jpg");
+  
+  /* Add the blur effect */
+  filter: blur(8px);
+  -webkit-filter: blur(8px);
+  
+  /* Full height */
+  height: 100%; 
+  
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+`
 
 function LoginPage () {
-  const [user, setUser] = useState('')
-  const [pass, setPass] = useState('')
-  const [profile, setProfile] = useState()
 
-  useEffect(() => {
-    getprofile()
-  }, [])
-
-  const login = async () => {
-    const result = await axios.post(
-      'http://localhost:3001/userloginpsu/login',
-      { username: user, password: pass }
-    )
-    console.log(result.data)
-    sessionStorage.setItem('login', JSON.stringify({ user: result.data }))
-  }
-
-  const getprofile = async () => {
-    const profiles = sessionStorage.getItem('login', JSON.stringify(user))
-    console.log('profiles', profiles)
-    setProfile(profiles)
-    //console.log('profile',profile)
-    //console.log(sessionStorage.getItem('login',JSON.stringify({user})))
-  }
   return (
     <StyledWrapper>
-      <div>
-        <h1>Login PSU!</h1>
-        <h3>
-          username :{' '}
-          <input type='text' onChange={e => setUser(e.target.value)}></input>{' '}
-        </h3>
-        <h3>
-          password :{' '}
-          <input type='text' onChange={e => setPass(e.target.value)}></input>{' '}
-        </h3>
-        <button type='submit' onClick={login}>
-          Login
-        </button>
-        <button type='submit' onClick={getprofile}>
-          Get profile
-        </button>
-        {profile}
-      </div>
+      <div class="bg-image"></div>
     </StyledWrapper>
   )
 }
