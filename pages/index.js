decc6d9d6f057cb0d71e2f0bccdd8ef740ff1a3e
alonecheer <alonecheer@hotmail.com>
2020-03-31 16:06:47 +0700

    test antd

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import LoginPage from '../src/components/Login'
+import { Button } from 'antd';
+import styled from 'styled-components';
+const StyledButton = styled(Button)`
+     color: red;
+`
 function HomePage() {
 
   return (
   <div>
+    <h3>Test Ant design</h3>
+     <StyledButton>Press Me</StyledButton>
     <LoginPage></LoginPage>
   </div>
   )
