b8d735330cdbdb759925355c40c1cb53f32b44ba
alonecheer <alonecheer@hotmail.com>
2020-03-31 15:45:47 +0700

    Chang Icon psu

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,7 @@
-import Document from 'next/document'
+import Document, { Html, Main, NextScript ,Head} from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
+import { render } from 'react-dom'
+
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -26,4 +28,19 @@ export default class MyDocument extends Document {
       sheet.seal()
     }
   }
+  render(){
+  return(
+    <Html>
+      <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      <link rel="icon" href="/static/images/psu_icon.png"></link>
+      </Head>
+      <body>
+        <Main/>
+        <NextScript/>
+      </body>
+    </Html>
+  )
 }
+}
+
