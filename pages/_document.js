df97216269c066eaa0c1b97c133aacd84c11abcf
alonecheer <alonecheer@hotmail.com>
2020-05-01 18:48:33 +0700

    Responsive meta tag

--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,16 @@
-import Document, { Html, Main, NextScript ,Head} from 'next/document'
+import Document, { Html, Main, NextScript, Head } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 import { render } from 'react-dom'
 
-
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps (ctx) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
         })
 
       const initialProps = await Document.getInitialProps(ctx)
@@ -22,29 +21,40 @@ export default class MyDocument extends Document {
             {initialProps.styles}
             {sheet.getStyleElement()}
           </>
-        ),
+        )
       }
     } finally {
       sheet.seal()
     }
   }
-  render(){
-  return(
-    <Html>
-      <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-      <link rel="icon" href="/static/images/psu_icon.png"></link>
-      <link rel="stylesheet" href="/static/styles/fonts.css"></link>
-      <link rel='stylesheet'   href='https://cdnjs.cloudflare.com/ajax/libs/antd/3.22.0/antd.min.css' />
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-      </Head>
-      <body>
-        <Main/>
-        <NextScript/>
-      </body>
-    </Html>
-  )
+  render () {
+    return (
+      <Html>
+        <Head>
+          <meta
+            name='viewport'
+            content='minimum-scale=1, initial-scale=1, width=device-width'
+          />
+          <link rel='icon' href='/static/images/psu_icon.png'></link>
+          <link rel='stylesheet' href='/static/styles/fonts.css'></link>
+          <link
+            rel='stylesheet'
+            href='https://cdnjs.cloudflare.com/ajax/libs/antd/3.22.0/antd.min.css'
+          />
+          <link
+            rel='stylesheet'
+            href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
+          />
+          <link
+            rel='stylesheet'
+            href='https://fonts.googleapis.com/icon?family=Material+Icons'
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
-}
-
