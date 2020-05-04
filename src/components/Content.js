08611f1164aafb54185a42fa2dc6ac7747a9af4b
alonecheer <alonecheer@hotmail.com>
2020-05-04 15:50:45 +0700

    homepage import content

--- src/components/Content.js
+++ src/components/Content.js
@@ -10,9 +10,6 @@ const { SubMenu } = Menu
 const { Header, Content, Sider, Footer } = Layout
 
 import styled from 'styled-components'
-import HeaderComponent from '../src/components/Header'
-import SiderComponent from '../src/components/Sider'
-import ContentComponent from '../src/components/Content'
 const StyledWrapper = styled.div`
   #components-layout-demo-top-side-2 .logo {
     width: 120px;
@@ -26,24 +23,26 @@ const StyledWrapper = styled.div`
     background: #fff;
   }
 `
-function HomePage () {
+
+const ContentComponent = () => {
   return (
     <StyledWrapper>
-      <title>HomePage</title>
-      <Layout>
-        <HeaderComponent />
-        <Layout>
-          <SiderComponent />
-          <Layout style={{ padding: '0 24px 24px' }}>
-            <ContentComponent />
-          </Layout>
-        </Layout>
-        <Footer style={{ textAlign: 'center' }}>
-          Copyright © 2020 CoE ระบบจัดซื้อ Purchasing system
-        </Footer>
-      </Layout>
+      <Breadcrumb style={{ margin: '16px 0' }}>
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>List</Breadcrumb.Item>
+        <Breadcrumb.Item>App</Breadcrumb.Item>
+      </Breadcrumb>
+      <Content
+        className='site-layout-background'
+        style={{
+          padding: 24,
+          margin: 0,
+          minHeight: 280
+        }}
+      >
+        Content
+      </Content>
     </StyledWrapper>
   )
 }
-
-export default HomePage
+export default ContentComponent
