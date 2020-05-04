dc22e585812b792b371b5deb87d84dbaf2a06e72
alonecheer <alonecheer@hotmail.com>
2020-05-04 15:08:27 +0700

    remove fontend

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,15 @@
-import LoginPage from '../src/components/Login'
+
 import styled from 'styled-components'
-import Router from 'next/router'
-import { useState, useEffect } from 'react'
-import WithAuth from '../src/hoc/withAuth'
-import MainPage from './main'
+import react , { useState, useEffect } from 'react'
 const StyledWrapper = styled.div`
 
 `
 function HomePage() {
-  const [token, setToken] = useState(null);
-
-  const CheckToken = async () => {
-    setToken(sessionStorage.getItem('token'))
-  }
-
-  useEffect(() => {
-    CheckToken()
-  }, [])
-
-  if(token === null){
-    return (
-      <>
-        <LoginPage />
-      </>
-    )
-  }
-  else{
-    return (
-      <>
-        <MainPage></MainPage>
-      </>
-    )
-  }
-  
+  return(
+    <StyledWrapper>
+      <title>Index</title>
+    </StyledWrapper>
+  )
 }
 
 export default HomePage
