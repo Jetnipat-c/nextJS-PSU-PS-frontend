294e8473809318421e768396548138419aad5eb7
alonecheer <alonecheer@hotmail.com>
2020-05-04 16:07:31 +0700

    getusername

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,21 +26,30 @@ const StyledWrapper = styled.div`
 `
 
 const HeaderComponent = () => {
-    const logout = () =>{
-        sessionStorage.removeItem('username')
-        sessionStorage.removeItem('token')
-        Router.push('/')
-    }
+  const logout = () => {
+    sessionStorage.removeItem('username')
+    sessionStorage.removeItem('token')
+    Router.push('/')
+  }
+  const [username, setUsername] = useState('')
+  const getuser = () => {
+    setUsername(sessionStorage.getItem('username'))
+  }
+  useEffect(()=>{
+      getuser()
+  },[])
   return (
     <StyledWrapper>
       <Header className='header'>
         <div className='logo' />
-        <Menu theme='dark' mode='horizontal' defaultSelectedKeys={['2']}>
+        <Menu theme='dark' mode='horizontal' defaultSelectedKeys={['1']}>
           <Menu.Item key='1'>nav 1</Menu.Item>
-          <Menu.Item key='2'>nav 2</Menu.Item>
-          <Menu.Item key='3'>
-          <a onClick={logout}>SignOut</a>
+          <Menu.Item key='2'>
+              {username}
               </Menu.Item>
+          <Menu.Item key='3'>
+            <a onClick={logout}>SignOut</a>
+          </Menu.Item>
         </Menu>
       </Header>
     </StyledWrapper>
