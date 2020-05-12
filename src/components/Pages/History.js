59c2b3f83cc5b487a232ff77b92146b8f4b58691
alonecheer <alonecheer@hotmail.com>
2020-05-12 23:48:59 +0700

    delete code someone

--- src/components/Pages/History.js
+++ src/components/Pages/History.js
@@ -13,9 +13,16 @@ import {
   Radio,
   PageHeader,
   Button,
-  Tabs, Statistic
+  Tabs,
+  Statistic
 } from 'antd'
-import { SearchOutlined,CloseCircleOutlined,DeleteOutlined,EditOutlined,PrinterOutlined    } from '@ant-design/icons'
+import {
+  SearchOutlined,
+  CloseCircleOutlined,
+  DeleteOutlined,
+  EditOutlined,
+  PrinterOutlined
+} from '@ant-design/icons'
 const { SubMenu } = Menu
 const { Header, Content, Sider, Footer } = Layout
 import styled from 'styled-components'
@@ -25,7 +32,7 @@ const StyledWrapper = styled.div`
     padding: 10px;
   }
   .ant-btn-three {
-    background-color: #58D68D;
+    background-color: #58d68d;
     color: white;
   }
   .ant-btn-sec {
@@ -46,11 +53,9 @@ const HistoryContent = () => {
     )
     console.log('found = ', found.data)
     setHistoty(JSON.parse(JSON.stringify(found.data)))
-    console.log('history',history)
+    console.log('history', history)
   }
-  const [showResults, setShowResults] = useState(false)
-  const showresult = () => setShowResults(true)
-  const closeesult = () => setShowResults(false)
+
   const [size, setSize] = useState('default')
   const cssonChange = e => {
     console.log('size checked', e.target.value)
@@ -107,12 +112,11 @@ const HistoryContent = () => {
                 <div key={index}>
                   <div className='site-page-header-ghost-wrapper'>
                     <PageHeader
-                    className="site-page-header-responsive"
+                      className='site-page-header-responsive'
                       ghost={false}
                       //onBack={() => window.history.back()}
                       title='ใบราายการที่'
                       subTitle={data.order_id}
-                      
                     >
                       <Descriptions size='small' column={3}>
                         <Descriptions.Item label='ผู้บันทึกใบรายการ'>
@@ -130,324 +134,44 @@ const HistoryContent = () => {
                       </Descriptions>
 
                       <Button
-                          key='6'
-                          type='three'
-                          style={{ marginLeft: '15px' }}
-                          onClick={() => view(data.order_id)}
-                          icon={<PrinterOutlined />}
-                        >
-                          View
-                        </Button>
+                        key='6'
+                        type='sec'
+                        style={{ marginLeft: '15px' }}
+                        onClick={() => view(data.order_id)}
+                        icon={<SearchOutlined />}
+                      >
+                        View
+                      </Button>
                       <Button
-                          key='5'
-                          type='three'
-                          style={{ marginLeft: '15px' }}
-                          onClick={() => genpdf(data.order_id)}
-                          icon={<PrinterOutlined />}
-                        >
-                          PDF
-                        </Button>
-                        <Button
-                          key='4'
-                          type='one'
-                          style={{ marginLeft: '15px' }}
-                          onClick={showresult}
-                          icon={<SearchOutlined />}
-                        >
-                          Show info
-                        </Button>
-                        <Button
-                          key='3'
-                          type='sec'
-                          style={{ marginLeft: '15px' }}
-                          onClick={closeesult}
-                          icon={<CloseCircleOutlined />}
-                           
-                        >
-                          Close info
-                        </Button>
-                        <Button
-                          key='2'
-                          type='danger'
-                          style={{ marginLeft: '15px' }}
-                          onClick={() => deletehistory(data.order_id)}
-                          icon={<DeleteOutlined  />}
-                        >
-                          Delete
-                        </Button>
-                        <Button
-                          key='1'
-                          type='primary'
-                          style={{ marginLeft: '15px' }}
-                          onClick={() => edithistory(data.order_id)}
-                          icon={<EditOutlined  />}
-                        >
-                          Edit
-                        </Button>
-                        
-                     
+                        key='5'
+                        type='three'
+                        style={{ marginLeft: '15px' }}
+                        onClick={() => genpdf(data.order_id)}
+                        icon={<PrinterOutlined />}
+                      >
+                        PDF
+                      </Button>
+
+                      <Button
+                        key='2'
+                        type='danger'
+                        style={{ marginLeft: '15px' }}
+                        onClick={() => deletehistory(data.order_id)}
+                        icon={<DeleteOutlined />}
+                      >
+                        Delete
+                      </Button>
+                      <Button
+                        key='1'
+                        type='primary'
+                        style={{ marginLeft: '15px' }}
+                        onClick={() => edithistory(data.order_id)}
+                        icon={<EditOutlined />}
+                      >
+                        Edit
+                      </Button>
                     </PageHeader>
                   </div>
-                  {showResults ? (
-                    <div>
-                      <div>
-                        <Radio.Group onChange={cssonChange} value={size}>
-                          <Radio value='default'>default</Radio>
-                          <Radio value='middle'>middle</Radio>
-                          <Radio value='small'>small</Radio>
-                        </Radio.Group>
-                        <br />
-                        <br />
-                        <Descriptions
-                          bordered
-                          title='1. รายละเอียด'
-                          size={size}
-                        >
-                          <Descriptions.Item label='เอกสารของ'>
-                            <input
-                              className='text-input'
-                              value='sid'
-                              value={data.sid}
-                              disabled={true}
-                            ></input>
-                          </Descriptions.Item>
-                          <Descriptions.Item label='เอกสารที่ มอ. 696 /'>
-                            <input
-                              className='text-input'
-                              value={data.o_location}
-                              disabled={true}
-                            ></input>
-                          </Descriptions.Item>
-                          <Descriptions.Item label='ลงวันที่'>
-                            <input
-                              className='text-input'
-                              value={data.o_date}
-                              disabled={true}
-                            ></input>
-                          </Descriptions.Item>
-                          <Descriptions.Item
-                            span={3}
-                            label='ด้วยมหาวิทยาลัยการคอมพิวเตอร์
-                มีความประสงค์ที่จะใช้พัสดุด้านล่างนี้เพื่อ'
-                          >
-                            <input
-                              className='text-input'
-                              value={data.o_purpose}
-                              disabled={true}
-                            ></input>
-                          </Descriptions.Item>
-                          <Descriptions.Item label='ชื่อโครงการ (ถ้ามี)'>
-                            <input
-                              className='text-input'
-                              value={data.o_projectname}
-                              disabled={true}
-                            ></input>
-                          </Descriptions.Item>
-                          <Descriptions.Item label='วันที่ต้องใช้พัสดุ'>
-                            <input
-                              className='text-input'
-                              value={data.o_require}
-                              disabled={true}
-                            ></input>
-                          </Descriptions.Item>
-                        </Descriptions>
-                        {/* -------------------------------------------------------------------------------------------------------------- */}
-                        <br></br>
-                        {/* ---------------------------------------------------- Ch 2 ---------------------------------------------------- */}
-                        <Descriptions
-                          bordered
-                          title='2. รายละเอียดคุณลักษณะเฉพาะ/ขอบเขตงาน'
-                          size={size}
-                        >
-                          <Descriptions.Item
-                            span={3}
-                            label='2.1 รายละเอียดคุณลักษณะเฉพาะ/ขอบเขตงาน
-                (ประทับตรามหาวิทยาลัยพร้อมผู้มีอำนาจลงนาม)  (จำนวนแผ่น)'
-                          >
-                            <input
-                              type='number'
-                              className='text-input'
-                              value={data.o_specific}
-                              disabled={true}
-                            ></input>
-                          </Descriptions.Item>
-                          <Descriptions.Item
-                            span={3}
-                            label='2.2
-                บันทึกการแต่งตั้งคณะกรรมการกำหนดคุณลักษณะเฉพาะ/ขอบเขตงานและราคากลาง  (จำนวนแผ่น)'
-                          >
-                            <input
-                              type='number'
-                              className='text-input'
-                              value={data.o_appointment}
-                              disabled={true}
-                            ></input>
-                          </Descriptions.Item>
-                          <Descriptions.Item
-                            span={3}
-                            label='2.3 บันทึกรายงานผลการจัดทำคุณลักษณะเฉพาะ/ขอบเขตงานและราคากลาง  (จำนวนแผ่น)'
-                          >
-                            <input
-                              type='number'
-                              className='text-input'
-                              value={data.o_results}
-                              disabled={true}
-                            ></input>
-                          </Descriptions.Item>
-                        </Descriptions>
-                        {/* -------------------------------------------------------------------------------------------------------------- */}
-                        <br />
-                        {/* ---------------------------------------------------- Ch 3 ---------------------------------------------------- */}
-                        <Descriptions
-                          bordered
-                          title='3. คณะกรรมการ'
-                          size={size}
-                        >
-                          <div span={3}>คณะกรรมการพิจารณาผล</div>
-                          <Descriptions.Item label='ประธานกรรมการ'>
-                            <input
-                              className='text-input'
-                              value={data.o_committee1}
-                              disabled={true}
-                            ></input>
-                          </Descriptions.Item>
-                          <Descriptions.Item label='คณะกรรมการ'>
-                            <input
-                              className='text-input'
-                              value={data.o_committee2}
-                              disabled={true}
-                            ></input>
-                          </Descriptions.Item>
-                          <Descriptions.Item label='คณะกรรมการ'>
-                            <input
-                              className='text-input'
-                              value={data.o_committee3}
-                              disabled={true}
-                            ></input>
-                          </Descriptions.Item>
-                          <div span={3}>คณะกรรมการตรวจรับพัสดุ</div>
-                          <Descriptions.Item label='ประธานกรรมการ'>
-                            <input
-                              className='text-input'
-                              value={data.o_committee4}
-                              disabled={true}
-                            ></input>
-                          </Descriptions.Item>
-                          <Descriptions.Item label='คณะกรรมการ'>
-                            <input
-                              className='text-input'
-                              value={data.o_committee5}
-                              disabled={true}
-                            ></input>
-                          </Descriptions.Item>
-                          <Descriptions.Item label='คณะกรรมการ'>
-                            <input
-                              className='text-input'
-                              value={data.o_committee6}
-                              disabled={true}
-                            ></input>
-                          </Descriptions.Item>
-                        </Descriptions>
-                        {/* -------------------------------------------------------------------------------------------------------------- */}
-                        <br />
-                        {/* ---------------------------------------------------- Ch 4 ---------------------------------------------------- */}
-                        <Descriptions bordered title='4. แหล่งเงิน' size={size}>
-                          <div span={3}>* ใส่ข้อมูลเฉพาะที่มี</div>
-                          <Descriptions.Item label='เงินอุดหนุนจากรัฐบาล ปี'>
-                            <input
-                              type='number'
-                              className='text-input'
-                              value={data.o_gvm_sub}
-                              disabled={true}
-                            ></input>
-                          </Descriptions.Item>
-                          <Descriptions.Item label='เงินรายได้ ปี'>
-                            <input
-                              type='number'
-                              className='text-input'
-                              value={data.o_income}
-                              disabled={true}
-                            ></input>
-                          </Descriptions.Item>
-                          <Descriptions.Item label='เงินรายได้สะสม ปี'>
-                            <input
-                              type='number'
-                              className='text-input'
-                              value={data.o_aml_income}
-                              disabled={true}
-                            ></input>
-                          </Descriptions.Item>
-
-                          <Descriptions.Item label='ทิศทาง'>
-                            <input
-                              className='text-input'
-                              value={data.o_direction}
-                              disabled={true}
-                            ></input>
-                          </Descriptions.Item>
-                          <Descriptions.Item span={2} label='นโยบาย'>
-                            <input
-                              className='text-input'
-                              value={data.o_policy}
-                              disabled={true}
-                            ></input>
-                          </Descriptions.Item>
-                          <Descriptions.Item label='ผลงาน'>
-                            <input
-                              className='text-input'
-                              value={data.o_works}
-                              disabled={true}
-                            ></input>
-                          </Descriptions.Item>
-                          <Descriptions.Item span={2} label='งาน'>
-                            <input
-                              className='text-input'
-                              value={data.o_task}
-                              disabled={true}
-                            ></input>
-                          </Descriptions.Item>
-                          <Descriptions.Item label='หมวดรายจ่าย'>
-                            <input
-                              className='text-input'
-                              value={data.o_expense_cg}
-                              disabled={true}
-                            ></input>
-                          </Descriptions.Item>
-                          <Descriptions.Item span={2} label='หมวดย่อย'>
-                            <input
-                              className='text-input'
-                              value={data.o_sub_cg}
-                              disabled={true}
-                            ></input>
-                          </Descriptions.Item>
-                          <Descriptions.Item span={3} label='ชื่อรายการ'>
-                            <input
-                              className='text-input'
-                              value={data.o_list_n}
-                              disabled={true}
-                            ></input>
-                          </Descriptions.Item>
-                          <Descriptions.Item
-                            span={3}
-                            label='เงินอื่นๆโปรดระบุ (ตามที่แนบมาพร้อมนี้) '
-                          >
-                            <input
-                              type='number'
-                              className='text-input'
-                              value={data.o_other_m}
-                              disabled={true}
-                            ></input>
-                          </Descriptions.Item>
-                        </Descriptions>
-                        <br />
-                        <Divider
-                          orientation='left'
-                          style={{ color: '#333', fontWeight: 'normal' }}
-                        ></Divider>
-                        {/* -------------------------------------------------------------------------------------------------------------- */}
-                      </div>
-                    </div>
-                  ) : null}
                 </div>
               )
             })}
