fedd7ed6201cf4f7b8e049d37c5a9aeee797e601
alonecheer <alonecheer@hotmail.com>
2020-05-13 01:45:19 +0700

    css A4 print

--- src/components/Pages/Genpdf.js
+++ src/components/Pages/Genpdf.js
@@ -3,7 +3,50 @@ import axios from 'axios'
 import ReactToPrint from 'react-to-print'
 import { useRouter } from 'next/router'
 import styled from 'styled-components'
-const StyledWrapper = styled.div``
+const StyledWrapper = styled.div`
+  margin: 0;
+  padding: 0;
+  background-color: #fafafa;
+  font: 12pt 'Tahoma';
+  * {
+    box-sizing: border-box;
+    -moz-box-sizing: border-box;
+  }
+
+  .page {
+    width: 21cm;
+    min-height: 29.7cm;
+    padding: 1cm;
+    margin: 1cm auto;
+    border: 1px #d3d3d3 solid;
+    border-radius: 5px;
+    background: white;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+  }
+
+  .subpage {
+    padding: 1cm;
+
+    height: 256mm;
+  
+  }
+
+ 
+
+  @media print {
+    .page {
+      margin: 0;
+      border: initial;
+      border-radius: initial;
+      width: initial;
+      min-height: initial;
+      box-shadow: initial;
+      background: initial;
+      page-break-after: always;
+    }
+  }
+`
+
 class ComponentToPrint extends React.Component {
   state = {
     info: []
@@ -22,26 +65,62 @@ class ComponentToPrint extends React.Component {
   //this.setState(JSON.parse(JSON.stringify(found.data)))
   render () {
     return (
-      <div>
-        
-          <h1>GenPDF Example</h1>
-          {this.state.info.map(data => (
-            <div key={data} >
-              <div >
-              <div> เอกสารที่ มอ. 696 / : {data.o_location}</div>
+      <StyledWrapper>
+        {this.state.info.map(data => (
+          <div key={data}>
+            <div className='book'>
+              <div className='page'>
+                <div className='subpage'>
+                  <div>
+                    <h1>GenPDF Example</h1>
+                    <div> เอกสารที่ มอ. 696 / : {data.o_location}</div>
                     <div> ลงวันที่ {data.o_date}</div>
-                    <div> ด้วยมหาวิทยาลัยการคอมพิวเตอร์มีความประสงค์ที่จะใช้พัสดุด้านล่างนี้เพื่อ {data.o_purpose}</div>
+                    <div>
+                      {' '}
+                      ด้วยมหาวิทยาลัยการคอมพิวเตอร์มีความประสงค์ที่จะใช้พัสดุด้านล่างนี้เพื่อ{' '}
+                      {data.o_purpose}
+                    </div>
                     <div> ชื่อโครงการ (ถ้ามี) {data.o_projectname}</div>
                     <div> วันที่ต้องใช้พัสดุ {data.o_require}</div>
-                    <div> รายละเอียดคุณลักษณะเฉพาะ/ขอบเขตงาน(ประทับตรามหาวิทยาลัยพร้อมผู้มีอำนาจลงนาม)  (จำนวนแผ่น) {data.o_specific}</div>
-                    <div> บันทึกการแต่งตั้งคณะกรรมการกำหนดคุณลักษณะเฉพาะ/ขอบเขตงานและราคากลาง  (จำนวนแผ่น) {data.o_appointment}</div>
-                    <div> บันทึกรายงานผลการจัดทำคุณลักษณะเฉพาะ/ขอบเขตงานและราคากลาง  (จำนวนแผ่น) {data.o_results}</div>
-                    <div> คณะกรรมการพิจารณาผล ประธานกรรมการ {data.o_committee1}</div>
-                    <div> คณะกรรมการพิจารณาผล คณะกรรมการ {data.o_committee2}</div>
-                    <div> คณะกรรมการพิจารณาผล คณะกรรมการ {data.o_committee3}</div>
-                    <div> คณะกรรมการตรวจรับพัสดุ ประธานกรรมการ {data.o_committee4}</div>
-                    <div> คณะกรรมการตรวจรับพัสดุ คณะกรรมการ {data.o_committee5}</div>
-                    <div> คณะกรรมการตรวจรับพัสดุ คณะกรรมการ {data.o_committee6}</div>
+                    <div>
+                      {' '}
+                      รายละเอียดคุณลักษณะเฉพาะ/ขอบเขตงาน(ประทับตรามหาวิทยาลัยพร้อมผู้มีอำนาจลงนาม)
+                      (จำนวนแผ่น) {data.o_specific}
+                    </div>
+                    <div>
+                      {' '}
+                      บันทึกการแต่งตั้งคณะกรรมการกำหนดคุณลักษณะเฉพาะ/ขอบเขตงานและราคากลาง
+                      (จำนวนแผ่น) {data.o_appointment}
+                    </div>
+                    <div>
+                      {' '}
+                      บันทึกรายงานผลการจัดทำคุณลักษณะเฉพาะ/ขอบเขตงานและราคากลาง
+                      (จำนวนแผ่น) {data.o_results}
+                    </div>
+                    <div>
+                      {' '}
+                      คณะกรรมการพิจารณาผล ประธานกรรมการ {data.o_committee1}
+                    </div>
+                    <div>
+                      {' '}
+                      คณะกรรมการพิจารณาผล คณะกรรมการ {data.o_committee2}
+                    </div>
+                    <div>
+                      {' '}
+                      คณะกรรมการพิจารณาผล คณะกรรมการ {data.o_committee3}
+                    </div>
+                    <div>
+                      {' '}
+                      คณะกรรมการตรวจรับพัสดุ ประธานกรรมการ {data.o_committee4}
+                    </div>
+                    <div>
+                      {' '}
+                      คณะกรรมการตรวจรับพัสดุ คณะกรรมการ {data.o_committee5}
+                    </div>
+                    <div>
+                      {' '}
+                      คณะกรรมการตรวจรับพัสดุ คณะกรรมการ {data.o_committee6}
+                    </div>
                     <div> เงินอุดหนุนจากรัฐบาล ปี {data.o_gvm_sub}</div>
                     <div> เงินรายได้ ปี {data.o_income}</div>
                     <div> เงินรายได้สะสม ปี {data.o_aml_income}</div>
@@ -52,12 +131,17 @@ class ComponentToPrint extends React.Component {
                     <div> หมวดรายจ่าย {data.o_expense_cg}</div>
                     <div> หมวดย่อย {data.o_sub_cg}</div>
                     <div> ชื่อรายการ {data.o_list_n}</div>
-                    <div> เงินอื่นๆโปรดระบุ (ตามที่แนบมาพร้อมนี้) {data.o_other_m}</div>
+                    <div>
+                      {' '}
+                      เงินอื่นๆโปรดระบุ (ตามที่แนบมาพร้อมนี้) {data.o_other_m}
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-          ))}
-       
-      </div>
+          </div>
+        ))}
+      </StyledWrapper>
     )
   }
 }
