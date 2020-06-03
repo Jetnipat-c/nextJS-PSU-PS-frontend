ea16901789f9303f60ee5b979e51709e455b7ff4
alonecheer <alonecheer@hotmail.com>
2020-06-03 11:00:41 +0700

    add reducer

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
 import { ProfileReducer } from './profile/reducer'
 import { Form001Reducer } from './form001/reducer'
+import { Form001_2Reducer } from './form001_2/reducer'
 export const reducers = combineReducers({
     Profile: ProfileReducer,
-    Form001: Form001Reducer
+    Form001: Form001Reducer,
+    Form001_2: Form001_2Reducer
 })
