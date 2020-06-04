39098a18b6a8aacdfd0dd528c155c1305e49f49e
alonecheer <alonecheer@hotmail.com>
2020-06-04 22:42:07 +0700

    antd uploadfile

--- src/components/Pages/Form001_Uploadfile.js
+++ src/components/Pages/Form001_Uploadfile.js
@@ -1,30 +1,52 @@
 import React, { useState, useEffect } from "react";
 import * as axios from "axios";
 import {
-    Layout,
-    Menu,
-    Breadcrumb,
-    message,
-    Divider,
-    Descriptions,
-    Radio,
-    BackTop,
-    Row,
-    Col,
-    Input,
-  } from "antd";
-  const { SubMenu } = Menu;
-  const { Header, Content, Sider, Footer } = Layout;
-  import styled from "styled-components";
-  const StyledWrapper = styled.div`
-  
-  `
+  Layout,
+  Menu,
+  Breadcrumb,
+  message,
+  Divider,
+  Descriptions,
+  Radio,
+  BackTop,
+  Row,
+  Col,
+  Input,
+  Upload,
+  Button,
+} from "antd";
+import { UploadOutlined } from '@ant-design/icons';
+const { SubMenu } = Menu;
+const { Header, Content, Sider, Footer } = Layout;
+import styled from "styled-components";
+const StyledWrapper = styled.div`
+
+`;
+
+const props_Uploadfile = {
+    action: '//jsonplaceholder.typicode.com/posts/',
+    listType: 'picture',
+    previewFile(file) {
+      console.log('Your upload file:', file);
+      // Your process logic. Here we just mock to the same file
+      return fetch('https://next.json-generator.com/api/json/get/4ytyBoLK8', {
+        method: 'POST',
+        body: file,
+      })
+        .then(res => res.json())
+        .then(({ thumbnail }) => thumbnail);
+    },
+  };
+
 const Form001_UploadfileContent = (props) => {
-    
-    return(
-        <StyledWrapper>
-            uploadfile
-        </StyledWrapper>
-    )
-}
+  return (
+    <StyledWrapper>
+      <Upload {...props_Uploadfile}>
+      <Button>
+        <UploadOutlined /> Upload
+      </Button>
+    </Upload>
+    </StyledWrapper>
+  );
+};
 export default Form001_UploadfileContent;
