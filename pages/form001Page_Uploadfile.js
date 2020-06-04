f7368ffc1a72d613d02c032c45e405d324393af8
alonecheer <alonecheer@hotmail.com>
2020-06-04 22:28:00 +0700

    create page uploadfile

--- pages/form001Page_Uploadfile.js
+++ pages/form001Page_Uploadfile.js
@@ -5,19 +5,19 @@ import HeaderComponent from '../src/components/Header'
 import SiderComponent from '../src/components/Sider'
 import FooterComponent from '../src/components/Footer'
 import WithAuth from '../src/hoc/withAuth'
-import Form001_page2Content from '../src/components/Pages/Form001_page2'
+import Form001_UploadfileContent from '../src/components/Pages/Form001_Uploadfile'
 import styled from 'styled-components'
 const StyledWrapper = styled.div``
-const Pageform001_2 = () => {
+const Pageform001_Uploadfile = () => {
   return (
     <StyledWrapper>
-      <title>Form001Page_2</title>
+      <title>Form001Page_Upload file</title>
       <Layout>
         <HeaderComponent />
         <Layout>
           <SiderComponent />
           <Layout style={{ padding: '0 24px 24px' }}>
-            <Form001_page2Content />
+            <Form001_UploadfileContent />
           </Layout>
         </Layout>
         <FooterComponent />
@@ -25,5 +25,6 @@ const Pageform001_2 = () => {
     </StyledWrapper>
   )
 }
-const WithTransfer = () => <WithAuth component={Pageform001_2} />
+const WithTransfer = () => <WithAuth component={Pageform001_Uploadfile} />
 export default WithTransfer
+
---
+++ src/components/Pages/Form001_Uploadfile.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import * as axios from "axios";
+import {
+    Layout,
+    Menu,
+    Breadcrumb,
+    message,
+    Divider,
+    Descriptions,
+    Radio,
+    BackTop,
+    Row,
+    Col,
+    Input,
+  } from "antd";
+  const { SubMenu } = Menu;
+  const { Header, Content, Sider, Footer } = Layout;
+  import styled from "styled-components";
+  const StyledWrapper = styled.div`
+  
+  `
+const Form001_UploadfileContent = (props) => {
+    
+    return(
+        <StyledWrapper>
+            uploadfile
+        </StyledWrapper>
+    )
+}
+export default Form001_UploadfileContent;
