6023bb13089deadf79543f377dbdaa759605c2b9
alonecheer <alonecheer@hotmail.com>
2020-06-19 19:15:16 +0700

    create table history

--- src/components/Pages/History.js
+++ src/components/Pages/History.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import Router from 'next/router'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Router from "next/router";
 import {
   Layout,
   Menu,
@@ -9,26 +9,24 @@ import {
   PageHeader,
   Button,
   Empty,
-  BackTop
-} from 'antd'
+  BackTop,
+  Table,
+  Space,
+  Tag,
+} from "antd";
 import {
   SearchOutlined,
   DeleteOutlined,
   EditOutlined,
-  PrinterOutlined
-} from '@ant-design/icons'
-const { SubMenu } = Menu
-const { Header, Content, Sider, Footer } = Layout
-import styled from 'styled-components'
+  PrinterOutlined,
+} from "@ant-design/icons";
+const { SubMenu } = Menu;
+const { Header, Content, Sider, Footer } = Layout;
+import styled from "styled-components";
 const StyledWrapper = styled.div`
-
-max-width: 960px;
+  max-width: 1200px;
   margin: 0 auto;
-  background-color:#f0f2f5;
-  .site-page-header-ghost-wrapper {
-    background-color: #f5f5f5;
-    padding: 10px;
-  }
+  background-color: #f0f2f5;
   .ant-btn-three {
     background-color: #58d68d;
     color: white;
@@ -41,168 +39,174 @@ max-width: 960px;
     background-color: #d8bfd8;
     color: white;
   }
-`
+`;
 const style = {
   height: 40,
   width: 40,
-  lineHeight: '40px',
+  lineHeight: "40px",
   borderRadius: 4,
-  backgroundColor: '#1088e9',
-  color: '#fff',
-  textAlign: 'center',
+  backgroundColor: "#1088e9",
+  color: "#fff",
+  textAlign: "center",
   fontSize: 14,
 };
 
 const HistoryContent = (props) => {
-  const [history, setHistoty] = useState([])
-  const [checkEmpty,setcCheckEmpty] = useState(false)
+  const [history, setHistoty] = useState([]);
+  const [checkEmpty, setcCheckEmpty] = useState(false);
   const getForm001Bysid = async () => {
     var found = await axios.get(
-      `http://localhost:3001/form001/${sessionStorage.getItem('username')}`
-    )
-    console.log('found = ', found.data)
-    setHistoty(JSON.parse(JSON.stringify(found.data)))
-    console.log('found length',(found.data).length);
-    if((found.data).length === 0)
-    {
-      setcCheckEmpty(true)
+      `http://localhost:3001/form001/${sessionStorage.getItem("username")}`
+    );
+    console.log("found = ", found.data);
+    setHistoty(JSON.parse(JSON.stringify(found.data)));
+    console.log("found length", found.data.length);
+    if (found.data.length === 0) {
+      setcCheckEmpty(true);
     }
-  }
+  };
 
-  const [size, setSize] = useState('default')
-  const cssonChange = e => {
-    console.log('size checked', e.target.value)
-    setSize(e.target.value)
-  }
+  const [size, setSize] = useState("default");
+  const cssonChange = (e) => {
+    console.log("size checked", e.target.value);
+    setSize(e.target.value);
+  };
   useEffect(() => {
-    getForm001Bysid()
-  }, [])
-  const deletehistory = async order_id => {
-    var found = await axios.delete(`http://localhost:3001/form001/${order_id}`)
-    Router.reload()
-  }
-  const edithistory = async order_id => {
+    getForm001Bysid();
+  }, []);
+  const deletehistory = async (order_id) => {
+    var found = await axios.delete(`http://localhost:3001/form001/${order_id}`);
+    Router.reload();
+  };
+  const edithistory = async (order_id) => {
     Router.push({
-      pathname: '/editform001page',
-      query: { order_id: order_id }
-    })
-  }
-  const genpdf = async order_id => {
+      pathname: "/editform001page",
+      query: { order_id: order_id },
+    });
+  };
+  const genpdf = async (order_id) => {
     Router.push({
-      pathname: '/genpdfpage',
-      query: { order_id: order_id }
-    })
-  }
-  const view = async order_id => {
+      pathname: "/genpdfpage",
+      query: { order_id: order_id },
+    });
+  };
+  const view = async (order_id) => {
     Router.push({
-      pathname: '/viewform001page',
-      query: { order_id: order_id }
-    })
- 
-  }
-  const [o_typedoc_name,setO_typedoc_name] = useState('')
-  const checkTypedoc = async o_typedoc => {
-    var result = await axios.get(`http://localhost:3001/typeform/${o_typedoc}`)
-    console.log('result = ',result.data)
-    setO_typedoc_name(result)
-  }
+      pathname: "/viewform001page",
+      query: { order_id: order_id },
+    });
+  };
+  const [o_typedoc_name, setO_typedoc_name] = useState("");
+  const checkTypedoc = async (o_typedoc) => {
+    var result = await axios.get(`http://localhost:3001/typeform/${o_typedoc}`);
+    console.log("result = ", result.data);
+    setO_typedoc_name(result);
+  };
+  const columns = [
+    {
+      title: "ใบรายการที่",
+      dataIndex: "order_id",
+      key: "order_id",
+      render: (text) => <a>{text}</a>,
+      align: "center",
+    },
+    {
+      title: "ผู้บันทึกรายการ",
+      dataIndex: "sid",
+      key: "sid",
+      responsive: ["md"],
+      align: "center",
+    },
+    {
+      title: "วันที่บันทึกใบรายการ",
+      dataIndex: "o_date",
+      key: "o_date",
+      responsive: ["lg"],
+      align: "center",
+    },
+    {
+      title: "ชื่อโครงการ",
+      dataIndex: "o_projectname",
+      key: "o_projectname",
+      responsive: ["lg"],
+    },
+    {
+      title: "สถานะ",
+      render: (record) => record.status_id.s_detail_en,
+      responsive: ["lg"],
+      align: "center",
+    },
+    {
+      title: "ประเภทใบรายการ",
+      render: (record) => record.typeform.o_typedoc_name,
+      responsive: ["lg"],
+      align: "center",
+    },
+    {
+      title: "ดำเนินการ",
+      render: (record) => (
+        <Space size="small">
+          <div>
+            <Button
+              key="6"
+              type="sec"
+              style={{ marginLeft: "15px" }}
+              onClick={() => view(record.order_id)}
+              icon={<SearchOutlined />}
+            >
+              View
+            </Button>
+            <Button
+              key="5"
+              type="three"
+              style={{ marginLeft: "15px" }}
+              onClick={() => genpdf(record.order_id)}
+              icon={<PrinterOutlined />}
+            >
+              PDF
+            </Button>
 
+            <Button
+              key="2"
+              type="danger"
+              style={{ marginLeft: "15px" }}
+              onClick={() => deletehistory(record.order_id)}
+              icon={<DeleteOutlined />}
+            >
+              Delete
+            </Button>
+            <Button
+              key="1"
+              type="primary"
+              style={{ marginLeft: "15px" }}
+              onClick={() => edithistory(record.order_id)}
+              icon={<EditOutlined />}
+            >
+              Edit
+            </Button>
+          </div>
+        </Space>
+      ),
+      responsive: ["md"],
+      align: "center",
+    },
+  ];
   return (
     <StyledWrapper>
-      <Breadcrumb style={{ margin: '16px 0' }}>
+      <Breadcrumb style={{ margin: "16px 0" }}>
         <Breadcrumb.Item>Home</Breadcrumb.Item>
         <Breadcrumb.Item>ประวัติการทำรายการ</Breadcrumb.Item>
       </Breadcrumb>
-    
-      <Content
-        className='site-layout-background'
-        style={{
-          padding: 24,
-          margin: 0,
-          minHeight: 280
-        }}
-      >
-        <div className='site-layout-content'>
-          <div className='site-page-header-ghost-wrapper'>
-            {checkEmpty ? <Empty/> : <div>{history.map((data, index) => {
-              return (
-                <div key={index}>
-                  <div className='site-page-header-ghost-wrapper'>
-                    <PageHeader
-                      className='site-page-header-responsive'
-                      ghost={false}
-                      //onBack={() => window.history.back()}
-                      title='ใบราายการที่'
-                      subTitle={data.order_id}
-                    >
-                      <Descriptions size='small' column={3}>
-                        <Descriptions.Item label='ผู้บันทึกใบรายการ'>
-                          {data.sid}
-                        </Descriptions.Item>
-                        <Descriptions.Item label='วันที่บันทึกใบรายการ'>
-                          {data.createdAt}
-                        </Descriptions.Item>
-                        <Descriptions.Item label='ชื่อโครงการ'>
-                          {data.o_projectname}
-                        </Descriptions.Item>
-                        <Descriptions.Item label='สถานะ'>{data.status_id.s_detail_th}</Descriptions.Item>
-                        <Descriptions.Item label='ประเภทใบรายการ'>
-                          {data.typeform.o_typedoc_name}
-                        </Descriptions.Item>
-                      </Descriptions>
-
-                      <Button
-                        key='6'
-                        type='sec'
-                        style={{ marginLeft: '15px' }}
-                        onClick={() => view(data.order_id)}
-                        icon={<SearchOutlined />}
-                      >
-                        View
-                      </Button>
-                      <Button
-                        key='5'
-                        type='three'
-                        style={{ marginLeft: '15px' }}
-                        onClick={() => genpdf(data.order_id)}
-                        icon={<PrinterOutlined />}
-                      >
-                        PDF
-                      </Button>
-
-                      <Button
-                        key='2'
-                        type='danger'
-                        style={{ marginLeft: '15px' }}
-                        onClick={() => deletehistory(data.order_id)}
-                        icon={<DeleteOutlined />}
-                      >
-                        Delete
-                      </Button>
-                      <Button
-                        key='1'
-                        type='primary'
-                        style={{ marginLeft: '15px' }}
-                        onClick={() => edithistory(data.order_id)}
-                        icon={<EditOutlined />}
-                      >
-                        Edit
-                      </Button>
-                    </PageHeader>
-                  </div>
-                </div>
-              )
-            })}</div>}
-
-            
-          </div>
-        </div>
-      </Content>
+      <Table
+        columns={columns}
+        dataSource={history}
+        rowKey={(record) => record.order_id}
+      />
       <BackTop>
         <div style={style}>UP</div>
       </BackTop>
     </StyledWrapper>
-  )
-}
+  );
+};
 
-export default HistoryContent
+export default HistoryContent;
