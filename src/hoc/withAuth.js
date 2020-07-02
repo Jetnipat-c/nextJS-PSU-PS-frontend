f9400f6d299e886db6161a1af48fb24cf447444b
alonecheer <alonecheer@hotmail.com>
2020-07-02 18:07:02 +0700

    test check type

--- src/hoc/withAuth.js
+++ src/hoc/withAuth.js
@@ -1,36 +1,48 @@
-import React,{ useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import LoginPage from "../components/Login";
-
+import axios from "axios";
 const WithAuth = (props) => {
+  const WithAuthComponent = props.component;
 
-    const WithAuthComponent = props.component;
+  const [token, setToken] = useState(null);
+  const [sidtype, setSidtype] = useState("");
+  console.log("sidtype ", sidtype);
+  const checktoken = async () => {
+    setToken(sessionStorage.getItem("token"));
+    const checktypenum = await axios.get(
+      `http://localhost:3001/users/checktype/${sessionStorage.getItem(
+        "username"
+      )}`
+    );
+    setSidtype(checktypenum.data[0].type_id);
+    console.log("result", checktypenum.data);
+  };
 
-    const [token, setToken] = useState(null);
-    const [sid, setSid] = useState('')
-    //console.log('sid ',sid)
-    const checktoken = async => {
-         setToken(sessionStorage.getItem('token'))
-         setSid(sessionStorage.getItem('username'))
-         
+  useEffect(() => {
+    checktoken();
+  }, []);
+
+  if (token === null) {
+    return (
+      <>
+        <LoginPage />
+      </>
+    );
+  }
+
+    else if (token != null && sidtype == 2){
+        return 'type 2'
     }
-     useEffect( () => {
-        checktoken()
-    }, [])
-
-
-    if (token === null) {
-        return (
-            <>
-                <LoginPage/>
-            </>
-        )
+    else if (token != null && sidtype == 1){
+        return 'type 1'
     }
-    else {
-        return (
-            <div><WithAuthComponent /></div>
-        )
-    }
-
-}
+  else {
+    return (
+      <div>
+        <WithAuthComponent />
+      </div>
+    );
+  }
+};
 
 export default WithAuth;
